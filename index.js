ca082aeb0311d7c53fba1fcadf33df6de3e43712
Hannah Lee <106423868+hannahlee2021@users.noreply.github.com>
2024-03-23 02:36:17 -0400

    third

--- index.js
+++ index.js
@@ -26,29 +26,22 @@ async function GetData() {
            
            for (let i = 0; i < books.length - 10; i++) {
 
-            titleData += `${books[i].title}<br>`;
-            authorData += `${books[i].author}<br>`;
-            bookImg += `${books[i].book_image}`;
-            //const book = books[i];
+           
+            const book = books[i];
             
-        //     content += `
-        //     <div>
-        //         <p>Title: ${book.title}</p>
-        //         <p>Author: ${book.author}</p>
-        //         <p>Description: ${book.description}
-        //         <img src="${book.book_image}" alt="${book.title}">
-        //     </div>
-        // `;
+            content += `
+            <div>
+                <p id ="title">Title: ${book.title}</p>
+                <p>Author: ${book.author}</p>
+                <p>Description: ${book.description}
+                <img src="${book.book_image}" alt="${book.title}">
+            </div>
+        `;
 
             
            }
-          //document.getElementById("data").innerHTML = content;
-          document.getElementById("title").innerHTML = titleData;
-          console.log(titleData);
-          document.getElementById("author").innerHTML = authorData;
-          const imgElement = document.getElementById("book-img");
-          imgElement.src = bookImg;
-          imgElement.style.display = "block";
+          document.getElementById("data").innerHTML = content;
+          
            
     
 
